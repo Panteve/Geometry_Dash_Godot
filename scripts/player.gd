c56d0c1c0739dd6d57ac869623a55b1e93edcf54
vTrxn <robinsonaranda29@gmail.com>
2026-07-30 18:23:10 -0500

    feat(cubo): agregar hitboxes para interacción (WIP)
    Se agregaron dos hitboxes al cubo para manejar la interacción y colisiones con pinchos y bloques. La funcionalidad aún sigue en desarrollo.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 
 func _physics_process(delta: float) -> void:
@@ -10,9 +10,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -500   
-		  
+		velocity.y = JUMP_VELOCITY
 		
 	velocity.x = SPEED
 
 	move_and_slide()
+
+
+func _on_hitbox_roja_area_entered(_area: Area2D) -> void:
+	die()
+
+
+func _on_hitbox_roja_body_entered(_body: Node2D) -> void:
+	die()
+
+
+func die() -> void:
+	get_tree().reload_current_scene()
