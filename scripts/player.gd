01d12879747faa3629d271f91ac6ba8da3c4e79a
Andres061308 <andreschiquiza08@gmail.com>
2026-07-30 08:58:16 -0500

    Script para el movimiento del personaje

---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -500   
+		  
+		
+	velocity.x = SPEED
+
+	move_and_slide()
