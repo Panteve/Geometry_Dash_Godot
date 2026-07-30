7bb29954e547fa5297155ef7a4fd5153d9891ec9
Andres061308 <andreschiquiza08@gmail.com>
2026-07-30 09:31:53 -0500

    (fix): Salto seguido solo cuando esta en el floor

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -500   
 		  
 		
