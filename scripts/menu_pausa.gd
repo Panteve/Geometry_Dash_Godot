1a6abcd951c8f972c3eddda794bc3d53e630a20e
Panteve <diegorojer@gmail.com>
2026-08-09 16:31:26 -0500

    fix:  boton para ir al menu ahora va al menu

--- scripts/menu_pausa.gd
+++ scripts/menu_pausa.gd
@@ -10,7 +10,7 @@ func _input(event):
 
 func _on_menu_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+	get_tree().change_scene_to_file("res://scenes/MenuPrincipal.tscn")
 
 func _on_play_pressed():
 	visible = false
