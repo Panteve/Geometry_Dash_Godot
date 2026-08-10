82d4d0dde1ae4dc9d11560a3a0a9db04952d271b
Andres061308 <andreschiquiza08@gmail.com>
2026-08-09 23:31:48 -0500

    Terminación del nivel y ajustes de velocidad

--- scripts/player.gd
+++ scripts/player.gd
@@ -39,7 +39,7 @@ func _physics_process(delta: float) -> void:
 				velocity.y = JUMP_VELOCITY
 		FORM.SHIP:
 			if Input.is_action_pressed("jump"):
-				velocity.y -= 1500.0 * delta
+				velocity.y -= 1600.0 * delta
 			velocity.y = clamp(velocity.y, -500.0, 300.0)
 			$Nave.rotation_degrees = clamp(velocity.y * 0.1, -25.0, 25.0)
 	velocity.x = SPEED
@@ -68,7 +68,7 @@ func set_mode():
 			current_form = FORM.CUBO
 
 func impulse(): 
-	velocity.y = JUMP_VELOCITY * 1.5
+	velocity.y = JUMP_VELOCITY * 1.8
 	
 func die():
 	if is_dead:
